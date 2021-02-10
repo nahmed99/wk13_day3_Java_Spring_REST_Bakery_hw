73d74575a8ac4c8948228cd5a516f188b8dd8c3b
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-02-10 15:22:25 +0000

    initial commit

---
+++ src/main/java/com/example/codeclan/breadtracker/BreadtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.breadtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreadtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeclan/breadtracker/BreadtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.breadtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
