62f4cddbfd47f5a7085040ce77bd226782e49b7a
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-02-10 17:24:12 +0000

    Add code to get bakeries from a particular region (case insensitive - although had to juggle it about a bit due to using enum to hold region values

--- src/main/java/com/example/codeclan/breadtracker/controllers/BakeryController.java
+++ src/main/java/com/example/codeclan/breadtracker/controllers/BakeryController.java
@@ -1,5 +1,6 @@
 package com.example.codeclan.breadtracker.controllers;
 
+import com.example.codeclan.breadtracker.helpers.Region;
 import com.example.codeclan.breadtracker.models.Bakery;
 import com.example.codeclan.breadtracker.repositories.BakeryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,9 +20,31 @@ public class BakeryController {
     @Autowired
     BakeryRepository bakeryRepository;
 
-    // REST GET all
+    /**
+     *  REST GET all, and Get Bakeries for a particular region
+     *
+     * GET /bakeries                        null            - findAll
+     * GET /bakeries?region=region_name     region_name     - findByRegion
+     */
+
     @GetMapping(value = "/bakeries")
-    public ResponseEntity<List<Bakery>> getAllBakeries() {
+    public ResponseEntity<List<Bakery>> getAllBakeries(
+            @RequestParam(name="region", required = false) String regionName
+    ) {
+
+        if (regionName != null) {
+            // If region name has been passed in...
+
+            // Convert whole string to lower case...
+            String cappedRegion = regionName.toLowerCase();
+            // and then capitalise the first letter of the string:
+            cappedRegion = cappedRegion.substring(0, 1).toUpperCase() + cappedRegion.substring(1);
+            //List<Bakery> allBakeries = bakeryRepository.findByRegionIgnoreCase(Region.valueOf(cappedRegion));
+            List<Bakery> allBakeries = bakeryRepository.findByRegion(Region.valueOf(cappedRegion));
+            return new ResponseEntity<>(allBakeries, HttpStatus.OK);
+        }
+
+        // GET ALL bakeries
         List<Bakery> allBakeries = bakeryRepository.findAll();
         return new ResponseEntity<>(allBakeries, HttpStatus.OK);
     }
@@ -30,7 +54,5 @@ public class BakeryController {
     public ResponseEntity<Optional<Bakery>> getBakery(@PathVariable Long id) {
         return new ResponseEntity<>(bakeryRepository.findById(id), HttpStatus.OK);
     }
-
-    // Get Bakeries for a particular region
 
 }
--- src/main/java/com/example/codeclan/breadtracker/repositories/BakeryRepository.java
+++ src/main/java/com/example/codeclan/breadtracker/repositories/BakeryRepository.java
@@ -1,9 +1,16 @@
 package com.example.codeclan.breadtracker.repositories;
 
+import com.example.codeclan.breadtracker.helpers.Region;
 import com.example.codeclan.breadtracker.models.Bakery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BakeryRepository extends JpaRepository<Bakery, Long> {
+
+    // Get all Bbakeries in a given region
+    // List<Bakery> findByRegionIgnoreCase(Region region);
+    List<Bakery> findByRegion(Region region);
 }
