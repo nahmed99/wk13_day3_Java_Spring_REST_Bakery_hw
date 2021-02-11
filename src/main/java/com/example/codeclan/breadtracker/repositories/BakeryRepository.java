6295d18230173e05cfa0909cd2995dc592b35694
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-02-11 10:13:19 +0000

    added comment to BakeryRepository, re: processing enum and strings

--- src/main/java/com/example/codeclan/breadtracker/repositories/BakeryRepository.java
+++ src/main/java/com/example/codeclan/breadtracker/repositories/BakeryRepository.java
@@ -13,6 +13,13 @@ public interface BakeryRepository extends JpaRepository<Bakery, Long> {
 
     // Get all Bbakeries in a given region
     // List<Bakery> findByRegionIgnoreCase(Region region);
+
+    // If I used the type as String instead of Region below, then I
+    // wouldn't have had to convert the incoming string (from http)
+    // to enum (in BakeryController - getAllBakeries())...could have
+    // just capitalised the 'word' and passed to this function. But(!)
+    // more importantly, I could have passed on 'IgnoreCase' functionality
+    // to Spring (sql). Not sure which is more resource expensive.
     List<Bakery> findByRegion(Region region);
 
 }
